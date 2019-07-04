09183815a589aaec5104c95836898d201656672d
Đào Tuấn Trung <termanteus@aos-iMac.local>
2019-07-05 01:12:33 +0700

    [DANGER]_FixLoginFolder

--- client/src/common/react/modals/Login/schema.js
+++
@@ -1,6 +0,0 @@
-import * as yup from "yup";
-
-export const accountSchema = yup.object().shape({
-  email: yup.string().email("Please input a valid email."),
-  password: yup.string().min(6, "Password must be at least 6-char long.")
-});
